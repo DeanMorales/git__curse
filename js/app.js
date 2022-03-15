90d63e6fe43c4aaee5e444d31dff224a94609c7d
DeanMorales <emmanuel.morales.hdez@gmail.com>
2022-03-15 17:45:50 -0600

    este es mi primer proyectio con github

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/**********************************************
+ * prime proyecto trabajando con git
+ * elabora: dean Morales
+ * 
+ * este proyecto sirve como primer ejemplo de como trabajar con el coontrolador 
+ * de versiones git
+ * 
+ */
+
+console.log('hola mundo');
